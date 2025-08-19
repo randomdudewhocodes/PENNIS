c74a3773c110be337bf428508b39da5ec4704d5d
a random guy <nguyen07112007@gmail.com>
2025-08-19 01:20:53 -0700

    visualization :D

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,107 @@
+#include "raylib.h"
 #include "pennis.hpp"
 #include <cstdlib>
 #include <chrono>
+#include <vector>
+#include <cmath>
+#include <iostream>
 
 int main()
 {
     try
     {
-        std::vector<uint32_t> layerSizes = {1, 4, 4, 4, 1};
-        std::vector<uint32_t> actTypes = {Tanh, Tanh, Tanh, None};
+        std::vector<uint32_t> layerSizes = {1, 4, 4, 4, 4, 1};
+        std::vector<uint32_t> actTypes = {Tanh, Tanh, Tanh, Tanh, None};
         AdamParams adamParams = {0.9f, 0.999f, 1e-8f, 0.01f};
 
         PENNIS pennis(50, layerSizes, actTypes, adamParams);
-        
+
         const int epochs = 5000;
+        int currentEpoch = 0;
 
-        auto start_time = std::chrono::high_resolution_clock::now();
-
-        std::vector<float> input, target;
-
+        std::vector<float> trainInput, trainTarget;
         for (int i = 0; i < 50; i++)
         {
-            float x = float(i) / 49 * 3.141593 * 2;
-
-            input.push_back(x);
-            target.push_back(sin(x));
+            float x = float(i) / 49 * 2 * 3.141593f;
+            trainInput.push_back(x);
+            trainTarget.push_back(sinf(x));
         }
 
-        pennis.uploadInputs(input);
-        pennis.uploadTargets(target);
+        pennis.uploadInputs(trainInput);
+        pennis.uploadTargets(trainTarget);
 
-        for(int epoch = 0; epoch < epochs; epoch++)
+        const int screenWidth = 800;
+        const int screenHeight = 600;
+
+        SetConfigFlags(FLAG_MSAA_4X_HINT);
+        InitWindow(screenWidth, screenHeight, "NN Training Visualizer");
+        SetTargetFPS(60);
+
+        const float scaleX = screenWidth / (2 * 3.141593f);
+        const float scaleY = screenWidth / (2 * 3.141593f);
+
+        std::vector<float> nnOutputs(screenWidth, 0.0f);
+        std::vector<float> targetOutputs(screenWidth, 0.0f);
+
+        for (int px = 0; px < screenWidth; px++)
         {
-            pennis.train();
+            float x = (float)px / scaleX;
+            targetOutputs[px] = sinf(x);
+        }
 
-            //if(epoch % 100 == 0 || epoch == epochs - 1)
+        while (!WindowShouldClose())
+        {
+            for (int i = 0; i < 50 && currentEpoch < epochs; i++)
             {
-                std::cout << "\nEpoch " << epoch << ":";
-                pennis.printArchitecture();
+                pennis.train();
+                currentEpoch++;
             }
+
+            for (int px = 0; px < screenWidth; px++)
+            {
+                float x = (float)px / scaleX;
+                std::vector<float> in = {x};
+                auto out = pennis.predict(in);
+                nnOutputs[px] = out[0];
+            }
+
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+
+            DrawLineEx({0, (float)screenHeight/2}, {(float)screenWidth, (float)screenHeight/2}, 1.5f, GRAY);
+
+            Vector2 prevTarget;
+            bool firstTarget = true;
+            for (int px = 0; px < screenWidth; px++)
+            {
+                float py = screenHeight/2 - targetOutputs[px] * scaleY;
+                Vector2 pt = {(float)px, py};
+
+                if (!firstTarget) DrawLineEx(prevTarget, pt, 2.5f, BLUE);
+                else firstTarget = false;
+
+                prevTarget = pt;
+            }
+
+            Vector2 prevNN;
+            bool firstNN = true;
+            for (int px = 0; px < screenWidth; px++)
+            {
+                float py = screenHeight/2 - nnOutputs[px] * scaleY;
+                Vector2 pt = {(float)px, py};
+
+                if (!firstNN) DrawLineEx(prevNN, pt, 2.5f, RED);
+                else firstNN = false;
+
+                prevNN = pt;
+            }
+
+            DrawText(TextFormat("Epoch: %d / %d", currentEpoch, epochs), 10, 10, 20, BLACK);
+
+            EndDrawing();
         }
 
-        auto end_time = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-
-        std::cout << "Training Time: " << duration.count() << " ms\n";
-        std::cout << "Inference After Training \n";
-        for (int i = 0; i < 100; i++)
-        {
-            float x = float(i) / 99 * 3.141593 * 2;
-            std::vector<float> input = {x};
-            auto output = pennis.predict(input);
-            std::cout << "(" << input[0] << ", " << output[0] << "), ";
-        }
+        CloseWindow();
     }
     catch (const std::exception& e)
     {
